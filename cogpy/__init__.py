14f131ac1f27ac5ea94a09b8cec0962584b0fc0e
Chenyu <57704488+chenyu-psy@users.noreply.github.com>
2024-11-01 19:49:07 -0500

    check the status of the caps lock key

--- cogpy/__init__.py
+++ cogpy/__init__.py
@@ -1,13 +1,13 @@
 from .layout import stimBoxes
 from .trial import trial
 from .instruction import instr_brief, instr_loop, instr_input
-from .utils import clean_cache
+from .utils import is_capslock_on
 
 __all__ = [
     "stimBoxes",
     "trial",
     "instr_brief",
     "instr_loop",
-    "clean_cache",
+    "is_capslock_on",
     "instr_input"
 ]
--- cogpy/utils.py
+++ cogpy/utils.py
@@ -1,15 +1,11 @@
 from pathlib import Path
+import tkinter as tk
 
-# clean cache
-def clean_cache():
-    '''
-    If global key for escaping does not work, 
-    clean cache might be helpful.
-    '''
-    try:
-        delete_path = Path.cwd() / '__pycache__'
-        for filename in delete_path.iterdir():
-            delete_file=delete_path / filename
-            delete_file.unlink()
-    except:
-        print('The cache files do not exist')
+
+#%% Create a hidden Tkinter root window once, at module load time
+root = tk.Tk()
+root.withdraw()  # Hide the main window
+
+def is_capslock_on():
+    """Check if Caps Lock is currently on."""
+    return root.tk.call('tk::capslock')
