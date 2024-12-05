31e8eec967bfe9956f9fc8681bc215ce99c2d70d
Chenyu <57704488+chenyu-psy@users.noreply.github.com>
2024-12-05 15:07:29 -0600

    reduce dependencies

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='https://github.com/chenyu-psy/cogpy.git',
     packages=find_packages(),
     install_requires=[
-        'psychopy',
+        'psychopy[core, event, visual]',
         'pandas'
     ],
     extras_require={
